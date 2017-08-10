ali007871/Waves

Preserve order status by id

src/main/scala/com/wavesplatform/matcher/model/OrderHistory.scala
@@ -32,14 +32,16 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
     val pairAddress = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
     Option(p.pairAddressToOrderIds.get(pairAddress)) match {
       case Some(prev) =>
-        log.info(s"savePairAddress prev.size: ${prev.size}")
-        if (!prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev + orderId)
-        if (prev.size > MaxOrdersPerAddress) {
-          prev.collectFirst { case id if getOrderStatus(id).isFinal => id}
-            .foreach{ id => deleteOrder(assetPair, address, id ) }
+        log.info(s"savePairAddress prev.size: ${prev.length}")
+        var r = prev
+        if (prev.length >= MaxOrdersPerAddress) {
+          val (p1, p2) = prev.span(!getOrderStatus(_).isInstanceOf[LimitOrder.Cancelled])
+          r = if (p2.isEmpty) p1 else p1 ++ p2.tail
+
         }
+        p.pairAddressToOrderIds.put(pairAddress, r :+ orderId)
       case _ =>
-        p.pairAddressToOrderIds.put(pairAddress, Set(orderId))
+        p.pairAddressToOrderIds.put(pairAddress, Array(orderId))
     }
     log.info(s"savePairAddress lasts: ${System.currentTimeMillis() - ts} ms")
   }
@@ -116,7 +118,7 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
 
   override def getOrdersByPairAndAddress(assetPair: AssetPair, address: String): Set[String] = {
     val pairAddressKey = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
-    Option(p.pairAddressToOrderIds.get(pairAddressKey)).map(_.take(MaxOrdersPerAddress)).getOrElse(Set())
+    Option(p.pairAddressToOrderIds.get(pairAddressKey)).map(_.takeRight(MaxOrdersPerAddress).toSet).getOrElse(Set())
   }
 
   override def getAllOrdersByAddress(address: String): Set[String] = {
@@ -132,7 +134,7 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
     val pairAddress = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
     Option(p.pairAddressToOrderIds.get(pairAddress)) match {
       case Some(prev) =>
-        if (prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev - orderId)
+        if (prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev.filterNot(_ == orderId))
       case _ =>
     }
   }

Decentral-America/node-scala

(cherry picked from commit f9e4278)

src/main/scala/com/wavesplatform/matcher/model/OrderHistory.scala
@@ -31,13 +31,15 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
     val pairAddress = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
     Option(p.pairAddressToOrderIds.get(pairAddress)) match {
       case Some(prev) =>
-        if (!prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev + orderId)
-        if (prev.size > MaxOrdersPerAddress) {
-          prev.collectFirst { case id if orderStatus(id).isFinal => id}
-            .foreach{ id => deleteOrder(assetPair, address, id ) }
+        var r = prev
+        if (prev.length >= MaxOrdersPerAddress) {
+          val (p1, p2) = prev.span(!orderStatus(_).isInstanceOf[LimitOrder.Cancelled])
+          r = if (p2.isEmpty) p1 else p1 ++ p2.tail
+
         }
+        p.pairAddressToOrderIds.put(pairAddress, r :+ orderId)
       case _ =>
-        p.pairAddressToOrderIds.put(pairAddress, Set(orderId))
+        p.pairAddressToOrderIds.put(pairAddress, Array(orderId))
     }
   }
 
@@ -109,7 +111,7 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
 
   override def ordersByPairAndAddress(assetPair: AssetPair, address: String): Set[String] = {
     val pairAddressKey = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
-    Option(p.pairAddressToOrderIds.get(pairAddressKey)).map(_.take(MaxOrdersPerAddress)).getOrElse(Set())
+    Option(p.pairAddressToOrderIds.get(pairAddressKey)).map(_.takeRight(MaxOrdersPerAddress).toSet).getOrElse(Set())
   }
 
   override def getAllOrdersByAddress(address: String): Set[String] = {
@@ -125,7 +127,7 @@ case class OrderHistoryImpl(p: OrderHistoryStorage) extends OrderHistory with Sc
     val pairAddress = OrderHistoryStorage.assetPairAddressKey(assetPair, address)
     Option(p.pairAddressToOrderIds.get(pairAddress)) match {
       case Some(prev) =>
-        if (prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev - orderId)
+        if (prev.contains(orderId)) p.pairAddressToOrderIds.put(pairAddress, prev.filterNot(_ == orderId))
       case _ =>
     }
   }