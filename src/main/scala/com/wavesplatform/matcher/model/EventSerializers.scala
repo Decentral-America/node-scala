tuanfish/Waves

DEX-153 Binary event serialization

src/main/scala/com/wavesplatform/matcher/model/EventSerializers.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.matcher.model
 
 import java.io.NotSerializableException
+import java.nio.ByteBuffer
 
 import akka.serialization._
 import com.wavesplatform.matcher.market.MatcherActor.OrderBookCreated
@@ -9,8 +10,7 @@ import com.wavesplatform.matcher.market.{MatcherActor, OrderBookActor}
 import com.wavesplatform.matcher.model.Events._
 import com.wavesplatform.matcher.model.MatcherModel.{Level, Price}
 import com.wavesplatform.metrics.TimerExt
-import com.wavesplatform.transaction.assets.exchange.OrderJson._
-import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order}
+import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order, OrderJson}
 import kamon.Kamon
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
@@ -24,8 +24,11 @@ class EventSerializers extends SerializerWithStringManifest {
   private val serializationTimer = Kamon.timer("matcher.serialization.encode")
   private val bytesWritten       = Kamon.histogram("matcher.serialization.bytes-written")
 
-  private def encodeAndMeasure[A: Writes](manifest: String, v: A): Array[Byte] = {
-    val bytes = serializationTimer.refine("manifest" -> manifest).measure(Json.toJson(v).toString().getBytes)
+  private def encodeAndMeasure[A: Writes](manifest: String, v: A): Array[Byte] =
+    encodeAndMeasure(manifest, Json.toJson(v).toString().getBytes)
+
+  private def encodeAndMeasure(manifest: String, f: => Array[Byte]): Array[Byte] = {
+    val bytes = serializationTimer.refine("manifest" -> manifest).measure(f)
     bytesWritten.refine("manifest" -> manifest).record(bytes.length)
     bytes
   }
@@ -34,9 +37,9 @@ class EventSerializers extends SerializerWithStringManifest {
   override def manifest(o: AnyRef): String = o match {
     case _: OrderBookActor.Snapshot       => Manifest.Snapshot
     case _: MatcherActor.OrderBookCreated => Manifest.OrderBookCreated
-    case _: OrderAdded                    => Manifest.OrderAdded
-    case _: OrderExecuted                 => Manifest.OrderExecuted
-    case _: OrderCanceled                 => Manifest.OrderCancelled
+    case _: OrderAdded                    => Manifest.OrderAddedV2
+    case _: OrderExecuted                 => Manifest.OrderExecutedV2
+    case _: OrderCanceled                 => Manifest.OrderCancelledV2
     case _: MatcherActor.Snapshot         => Manifest.MatcherSnapshot
   }
 
@@ -44,18 +47,21 @@ class EventSerializers extends SerializerWithStringManifest {
     case s: OrderBookActor.Snapshot         => encodeAndMeasure("order-book-snapshot", s)
     case obc: MatcherActor.OrderBookCreated => encodeAndMeasure("order-book-created", obc)
     case x: MatcherActor.Snapshot           => encodeAndMeasure("matcher-actor-snapshot", x)
-    case oa: OrderAdded                     => encodeAndMeasure("order-added", oa)
-    case oe: OrderExecuted                  => encodeAndMeasure("order-executed", oe)
-    case oc: OrderCanceled                  => encodeAndMeasure("order-cancelled", oc)
+    case oa: OrderAdded                     => encodeAndMeasure("order-added", encodeOrder(oa.order))
+    case oe: OrderExecuted                  => encodeAndMeasure("order-executed", encodeOrderExecuted(oe))
+    case oc: OrderCanceled                  => encodeAndMeasure("order-cancelled", encodeOrderCancelled(oc))
   }
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
-    case Manifest.Snapshot         => snapshotFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderBookCreated => orderBookCreatedFormat.reads(Json.parse(bytes)).get
-    case Manifest.MatcherSnapshot  => matcherSnapshot.reads(Json.parse(bytes)).get
-    case Manifest.OrderAdded       => orderAddedFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderExecuted    => orderExecutedFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderCancelled   => orderCancelledFormat.reads(Json.parse(bytes)).get
+    case Manifest.Snapshot         => parse[Snapshot](bytes)
+    case Manifest.OrderBookCreated => parse[OrderBookCreated](bytes)
+    case Manifest.MatcherSnapshot  => parse[MatcherActor.Snapshot](bytes)
+    case Manifest.OrderAddedV1     => parse[OrderAdded](bytes)
+    case Manifest.OrderAddedV2     => decodeOrder(bytes)
+    case Manifest.OrderExecutedV1  => parse[OrderExecuted](bytes)
+    case Manifest.OrderExecutedV2  => decodeOrderExecuted(bytes)
+    case Manifest.OrderCancelledV1 => parse[OrderCanceled](bytes)
+    case Manifest.OrderCancelledV2 => decodeOrderCancelled(bytes)
     case _                         => throw new NotSerializableException(manifest)
   }
 }
@@ -67,32 +73,35 @@ object EventSerializers {
     val Snapshot         = "snapshot"
     val OrderBookCreated = "orderBookCreated"
     val MatcherSnapshot  = "matcherSnapshot"
-    val OrderAdded       = "event.OrderAdded"
-    val OrderExecuted    = "event.OrderExecuted"
-    val OrderCancelled   = "event.OrderCancelled"
+
+    val OrderAddedV1 = "event.OrderAdded"
+    val OrderAddedV2 = "event.OrderAdded.v2"
+
+    val OrderExecutedV1 = "event.OrderExecuted"
+    val OrderExecutedV2 = "event.OrderExecuted.v2"
+
+    val OrderCancelledV1 = "event.OrderCancelled"
+    val OrderCancelledV2 = "event.OrderCancelled.v2"
   }
 
-  private def dataToSerialize(lo: LimitOrder) = (lo.amount, lo.price, lo.fee, lo.order)
+  private def parse[A: Reads](bytes: Array[Byte]): A = Json.parse(bytes).as[A]
 
-  private def limitOrderFormatBuilder[T <: LimitOrder](limitOrderBuilder: (Long, Long, Long, Order) => T): Format[T] = Format(
-    Reads[T] {
-      case js: JsObject =>
-        val amount = (js \ "amount").as[Long]
-        val price  = (js \ "price").as[Long]
-        val order  = (js \ "order").as[Order]
-        val fee    = (js \ "fee").asOpt[Long].getOrElse(LimitOrder.getPartialFee(order.matcherFee, order.amount, amount))
-        JsSuccess(limitOrderBuilder(amount, price, fee, order))
-      case _ => JsError("failed to deserialize LimitOrder")
-    },
-    ((__ \ "amount").format[Long] and
-      (__ \ "price").format[Long] and
-      (__ \ "fee").format[Long] and
-      (__ \ "order").format[Order])(limitOrderBuilder, dataToSerialize)
+  private case class LO(amount: Long, fee: Option[Long], order: Order) {
+    lazy val strictFee      = fee.getOrElse(LimitOrder.getPartialFee(order.matcherFee, order.amount, amount))
+    def toLimit: LimitOrder = LimitOrder.limitOrder(amount, strictFee, order)
+  }
+
+  implicit val orderFormat: Format[Order]   = OrderJson.orderFormat
+  private implicit val loFormat: Format[LO] = Json.format
+
+  private def limitOrderFormatBuilder[T <: LimitOrder](f: LO => T): Format[T] = Format(
+    js => JsSuccess(f(js.as[LO])),
+    lo => Json.toJson(LO(lo.amount, Some(lo.fee), lo.order))
   )
 
-  implicit val limitFormat: Format[LimitOrder]    = limitOrderFormatBuilder[LimitOrder](LimitOrder.limitOrder)
-  implicit val buyFormat: Format[BuyLimitOrder]   = limitOrderFormatBuilder[BuyLimitOrder](BuyLimitOrder.apply)
-  implicit val sellFormat: Format[SellLimitOrder] = limitOrderFormatBuilder[SellLimitOrder](SellLimitOrder.apply)
+  implicit val limitFormat: Format[LimitOrder]    = limitOrderFormatBuilder(_.toLimit)
+  implicit val buyFormat: Format[BuyLimitOrder]   = limitOrderFormatBuilder(lo => BuyLimitOrder(lo.amount, lo.strictFee, lo.order))
+  implicit val sellFormat: Format[SellLimitOrder] = limitOrderFormatBuilder(lo => SellLimitOrder(lo.amount, lo.strictFee, lo.order))
 
   implicit val orderMapWrites: Writes[Map[Price, Level[LimitOrder]]] = (tree: Map[Price, Level[LimitOrder]]) =>
     JsObject(tree.map {
@@ -112,24 +121,10 @@ object EventSerializers {
 
   implicit val orderBookFormat: Format[OrderBook] = Json.format
 
-  implicit val orderAddedFormat: Format[OrderAdded] = Format(
-    (__ \ "o").read[LimitOrder].map(OrderAdded),
-    Writes[OrderAdded](oa => Json.obj("o" -> oa.order))
-  )
-
-  implicit val orderExecutedFormat: Format[OrderExecuted] = ((__ \ "o1").format[LimitOrder] and
-    (__ \ "o2").format[LimitOrder])(OrderExecuted.apply, unlift(OrderExecuted.unapply))
-
-  implicit val orderCancelledFormat: Format[OrderCanceled] = Format(
-    Reads[OrderCanceled] {
-      case js: JsObject =>
-        val o = (js \ "o").as[LimitOrder]
-        val u = (js \ "unmatchable").asOpt[Boolean]
-        JsSuccess(OrderCanceled(o, unmatchable = u.getOrElse(false)))
-      case _ => JsError("failed to deserialize OrderCanceled")
-    },
-    Writes[OrderCanceled](oc => Json.obj("o" -> oc.limitOrder, "unmatchable" -> oc.unmatchable))
-  )
+  implicit val orderAddedReads: Reads[OrderAdded]        = js => JsSuccess(OrderAdded((js \ "o").as[LimitOrder]))
+  implicit val orderExecutedFormat: Reads[OrderExecuted] = js => JsSuccess(OrderExecuted((js \ "o1").as[LimitOrder], (js \ "o2").as[LimitOrder]))
+  implicit val orderCancelledFormat: Reads[OrderCanceled] = js =>
+    JsSuccess(OrderCanceled((js \ "o").as[LimitOrder], (js \ "unmatchable").asOpt[Boolean].getOrElse(false)))
 
   private def mkOrderBookCreated(a1: String, a2: String) = OrderBookCreated(AssetPair.createAssetPair(a1, a2).get)
   private def orderBookToPair(obc: OrderBookCreated)     = (obc.pair.amountAssetStr, obc.pair.priceAssetStr)
@@ -142,15 +137,6 @@ object EventSerializers {
 
   implicit val matcherSnapshot: Format[MatcherActor.Snapshot] = Json.format[MatcherActor.Snapshot]
 
-  implicit val tuple2Format: Format[(Price, Price)] = new Format[(Long, Long)] {
-    def writes(o: (Long, Long)): JsValue = Json.arr(o._1, o._2)
-
-    def reads(json: JsValue): JsResult[(Long, Long)] = {
-      val a = json.as[JsArray].value
-      JsSuccess((a.head.as[Long], a(1).as[Long]))
-    }
-  }
-
   implicit val cacheFormat: Format[Map[String, (Price, Price)]] = new Format[Map[String, (Long, Long)]] {
     def writes(cache: Map[String, (Long, Long)]): JsValue =
       JsObject(cache.mapValues(v => Json.arr(v._1, v._2)))
@@ -161,4 +147,51 @@ object EventSerializers {
 
   implicit val snapshotFormat: Format[Snapshot] =
     Format((JsPath \ "o").read[OrderBook].map(Snapshot), Writes[Snapshot](s => Json.obj("o" -> s.orderBook)))
+
+  private def encodeOrder(o: LimitOrder) = {
+    val orderBytes = 1.toByte +: o.order.bytes() // todo: change this to support orders v2+
+    ByteBuffer
+      .allocate(orderBytes.length + 16)
+      .putLong(o.amount)
+      .putLong(o.fee)
+      .put(orderBytes)
+      .array()
+  }
+
+  private def decodeOrder(bytes: Array[Byte]) = {
+    val buf        = ByteBuffer.wrap(bytes)
+    val amount     = buf.getLong
+    val fee        = buf.getLong
+    val orderBytes = bytes.drop(16)
+    val order = orderBytes.head match {
+      // todo: support orders v2+
+      case 1 => Order.parseBytes(orderBytes.tail).get
+    }
+    LimitOrder.limitOrder(amount, fee, order)
+  }
+
+  private def encodeOrderExecuted(oe: OrderExecuted) = {
+    val submittedBytes = encodeOrder(oe.submitted)
+    val counterBytes   = encodeOrder(oe.counter)
+
+    ByteBuffer
+      .allocate(8 + submittedBytes.length + counterBytes.length)
+      .putInt(submittedBytes.length)
+      .put(submittedBytes)
+      .putInt(counterBytes.length)
+      .put(counterBytes)
+      .array()
+  }
+
+  private def decodeOrderExecuted(bytes: Array[Byte]) = {
+    val bb        = ByteBuffer.wrap(bytes)
+    val submitted = new Array[Byte](bb.getInt)
+    bb.get(submitted)
+    val counter = new Array[Byte](bb.getInt)
+    bb.get(counter)
+    OrderExecuted(decodeOrder(submitted), decodeOrder(counter))
+  }
+
+  private def encodeOrderCancelled(oc: OrderCanceled)  = (if (oc.unmatchable) 1 else 0).toByte +: encodeOrder(oc.limitOrder)
+  private def decodeOrderCancelled(bytes: Array[Byte]) = OrderCanceled(decodeOrder(bytes.tail), bytes.head == 1)
 }

Decentral-America/node-scala

src/main/scala/com/wavesplatform/matcher/model/EventSerializers.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.matcher.model
 
 import java.io.NotSerializableException
+import java.nio.ByteBuffer
 
 import akka.serialization._
 import com.wavesplatform.matcher.market.MatcherActor.OrderBookCreated
@@ -9,8 +10,7 @@ import com.wavesplatform.matcher.market.{MatcherActor, OrderBookActor}
 import com.wavesplatform.matcher.model.Events._
 import com.wavesplatform.matcher.model.MatcherModel.{Level, Price}
 import com.wavesplatform.metrics.TimerExt
-import com.wavesplatform.transaction.assets.exchange.OrderJson._
-import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order}
+import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order, OrderJson}
 import kamon.Kamon
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
@@ -24,8 +24,11 @@ class EventSerializers extends SerializerWithStringManifest {
   private val serializationTimer = Kamon.timer("matcher.serialization.encode")
   private val bytesWritten       = Kamon.histogram("matcher.serialization.bytes-written")
 
-  private def encodeAndMeasure[A: Writes](manifest: String, v: A): Array[Byte] = {
-    val bytes = serializationTimer.refine("manifest" -> manifest).measure(Json.toJson(v).toString().getBytes)
+  private def encodeAndMeasure[A: Writes](manifest: String, v: A): Array[Byte] =
+    encodeAndMeasure(manifest, Json.toJson(v).toString().getBytes)
+
+  private def encodeAndMeasure(manifest: String, f: => Array[Byte]): Array[Byte] = {
+    val bytes = serializationTimer.refine("manifest" -> manifest).measure(f)
     bytesWritten.refine("manifest" -> manifest).record(bytes.length)
     bytes
   }
@@ -34,9 +37,9 @@ class EventSerializers extends SerializerWithStringManifest {
   override def manifest(o: AnyRef): String = o match {
     case _: OrderBookActor.Snapshot       => Manifest.Snapshot
     case _: MatcherActor.OrderBookCreated => Manifest.OrderBookCreated
-    case _: OrderAdded                    => Manifest.OrderAdded
-    case _: OrderExecuted                 => Manifest.OrderExecuted
-    case _: OrderCanceled                 => Manifest.OrderCancelled
+    case _: OrderAdded                    => Manifest.OrderAddedV2
+    case _: OrderExecuted                 => Manifest.OrderExecutedV2
+    case _: OrderCanceled                 => Manifest.OrderCancelledV2
     case _: MatcherActor.Snapshot         => Manifest.MatcherSnapshot
   }
 
@@ -44,18 +47,21 @@ class EventSerializers extends SerializerWithStringManifest {
     case s: OrderBookActor.Snapshot         => encodeAndMeasure("order-book-snapshot", s)
     case obc: MatcherActor.OrderBookCreated => encodeAndMeasure("order-book-created", obc)
     case x: MatcherActor.Snapshot           => encodeAndMeasure("matcher-actor-snapshot", x)
-    case oa: OrderAdded                     => encodeAndMeasure("order-added", oa)
-    case oe: OrderExecuted                  => encodeAndMeasure("order-executed", oe)
-    case oc: OrderCanceled                  => encodeAndMeasure("order-cancelled", oc)
+    case oa: OrderAdded                     => encodeAndMeasure("order-added", encodeOrderAdded(oa))
+    case oe: OrderExecuted                  => encodeAndMeasure("order-executed", encodeOrderExecuted(oe))
+    case oc: OrderCanceled                  => encodeAndMeasure("order-cancelled", encodeOrderCancelled(oc))
   }
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
-    case Manifest.Snapshot         => snapshotFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderBookCreated => orderBookCreatedFormat.reads(Json.parse(bytes)).get
-    case Manifest.MatcherSnapshot  => matcherSnapshot.reads(Json.parse(bytes)).get
-    case Manifest.OrderAdded       => orderAddedFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderExecuted    => orderExecutedFormat.reads(Json.parse(bytes)).get
-    case Manifest.OrderCancelled   => orderCancelledFormat.reads(Json.parse(bytes)).get
+    case Manifest.Snapshot         => parse[Snapshot](bytes)
+    case Manifest.OrderBookCreated => parse[OrderBookCreated](bytes)
+    case Manifest.MatcherSnapshot  => parse[MatcherActor.Snapshot](bytes)
+    case Manifest.OrderAddedV1     => parse[OrderAdded](bytes)
+    case Manifest.OrderAddedV2     => decodeOrderAdded(bytes)
+    case Manifest.OrderExecutedV1  => parse[OrderExecuted](bytes)
+    case Manifest.OrderExecutedV2  => decodeOrderExecuted(bytes)
+    case Manifest.OrderCancelledV1 => parse[OrderCanceled](bytes)
+    case Manifest.OrderCancelledV2 => decodeOrderCancelled(bytes)
     case _                         => throw new NotSerializableException(manifest)
   }
 }
@@ -67,32 +73,35 @@ object EventSerializers {
     val Snapshot         = "snapshot"
     val OrderBookCreated = "orderBookCreated"
     val MatcherSnapshot  = "matcherSnapshot"
-    val OrderAdded       = "event.OrderAdded"
-    val OrderExecuted    = "event.OrderExecuted"
-    val OrderCancelled   = "event.OrderCancelled"
+
+    val OrderAddedV1 = "event.OrderAdded"
+    val OrderAddedV2 = "event.OrderAdded.v2"
+
+    val OrderExecutedV1 = "event.OrderExecuted"
+    val OrderExecutedV2 = "event.OrderExecuted.v2"
+
+    val OrderCancelledV1 = "event.OrderCancelled"
+    val OrderCancelledV2 = "event.OrderCancelled.v2"
   }
 
-  private def dataToSerialize(lo: LimitOrder) = (lo.amount, lo.price, lo.fee, lo.order)
+  private def parse[A: Reads](bytes: Array[Byte]): A = Json.parse(bytes).as[A]
 
-  private def limitOrderFormatBuilder[T <: LimitOrder](limitOrderBuilder: (Long, Long, Long, Order) => T): Format[T] = Format(
-    Reads[T] {
-      case js: JsObject =>
-        val amount = (js \ "amount").as[Long]
-        val price  = (js \ "price").as[Long]
-        val order  = (js \ "order").as[Order]
-        val fee    = (js \ "fee").asOpt[Long].getOrElse(LimitOrder.getPartialFee(order.matcherFee, order.amount, amount))
-        JsSuccess(limitOrderBuilder(amount, price, fee, order))
-      case _ => JsError("failed to deserialize LimitOrder")
-    },
-    ((__ \ "amount").format[Long] and
-      (__ \ "price").format[Long] and
-      (__ \ "fee").format[Long] and
-      (__ \ "order").format[Order])(limitOrderBuilder, dataToSerialize)
+  private case class LO(amount: Long, fee: Option[Long], order: Order) {
+    lazy val strictFee      = fee.getOrElse(LimitOrder.getPartialFee(order.matcherFee, order.amount, amount))
+    def toLimit: LimitOrder = LimitOrder.limitOrder(amount, strictFee, order)
+  }
+
+  implicit val orderFormat: Format[Order]   = OrderJson.orderFormat
+  private implicit val loFormat: Format[LO] = Json.format
+
+  private def limitOrderFormatBuilder[T <: LimitOrder](f: LO => T): Format[T] = Format(
+    js => JsSuccess(f(js.as[LO])),
+    lo => Json.toJson(LO(lo.amount, Some(lo.fee), lo.order))
   )
 
-  implicit val limitFormat: Format[LimitOrder]    = limitOrderFormatBuilder[LimitOrder](LimitOrder.limitOrder)
-  implicit val buyFormat: Format[BuyLimitOrder]   = limitOrderFormatBuilder[BuyLimitOrder](BuyLimitOrder.apply)
-  implicit val sellFormat: Format[SellLimitOrder] = limitOrderFormatBuilder[SellLimitOrder](SellLimitOrder.apply)
+  implicit val limitFormat: Format[LimitOrder]    = limitOrderFormatBuilder(_.toLimit)
+  implicit val buyFormat: Format[BuyLimitOrder]   = limitOrderFormatBuilder(lo => BuyLimitOrder(lo.amount, lo.strictFee, lo.order))
+  implicit val sellFormat: Format[SellLimitOrder] = limitOrderFormatBuilder(lo => SellLimitOrder(lo.amount, lo.strictFee, lo.order))
 
   implicit val orderMapWrites: Writes[Map[Price, Level[LimitOrder]]] = (tree: Map[Price, Level[LimitOrder]]) =>
     JsObject(tree.map {
@@ -112,24 +121,10 @@ object EventSerializers {
 
   implicit val orderBookFormat: Format[OrderBook] = Json.format
 
-  implicit val orderAddedFormat: Format[OrderAdded] = Format(
-    (__ \ "o").read[LimitOrder].map(OrderAdded),
-    Writes[OrderAdded](oa => Json.obj("o" -> oa.order))
-  )
-
-  implicit val orderExecutedFormat: Format[OrderExecuted] = ((__ \ "o1").format[LimitOrder] and
-    (__ \ "o2").format[LimitOrder])(OrderExecuted.apply, unlift(OrderExecuted.unapply))
-
-  implicit val orderCancelledFormat: Format[OrderCanceled] = Format(
-    Reads[OrderCanceled] {
-      case js: JsObject =>
-        val o = (js \ "o").as[LimitOrder]
-        val u = (js \ "unmatchable").asOpt[Boolean]
-        JsSuccess(OrderCanceled(o, unmatchable = u.getOrElse(false)))
-      case _ => JsError("failed to deserialize OrderCanceled")
-    },
-    Writes[OrderCanceled](oc => Json.obj("o" -> oc.limitOrder, "unmatchable" -> oc.unmatchable))
-  )
+  implicit val orderAddedReads: Reads[OrderAdded]        = js => JsSuccess(OrderAdded((js \ "o").as[LimitOrder]))
+  implicit val orderExecutedFormat: Reads[OrderExecuted] = js => JsSuccess(OrderExecuted((js \ "o1").as[LimitOrder], (js \ "o2").as[LimitOrder]))
+  implicit val orderCancelledFormat: Reads[OrderCanceled] = js =>
+    JsSuccess(OrderCanceled((js \ "o").as[LimitOrder], (js \ "unmatchable").asOpt[Boolean].getOrElse(false)))
 
   private def mkOrderBookCreated(a1: String, a2: String) = OrderBookCreated(AssetPair.createAssetPair(a1, a2).get)
   private def orderBookToPair(obc: OrderBookCreated)     = (obc.pair.amountAssetStr, obc.pair.priceAssetStr)
@@ -142,15 +137,6 @@ object EventSerializers {
 
   implicit val matcherSnapshot: Format[MatcherActor.Snapshot] = Json.format[MatcherActor.Snapshot]
 
-  implicit val tuple2Format: Format[(Price, Price)] = new Format[(Long, Long)] {
-    def writes(o: (Long, Long)): JsValue = Json.arr(o._1, o._2)
-
-    def reads(json: JsValue): JsResult[(Long, Long)] = {
-      val a = json.as[JsArray].value
-      JsSuccess((a.head.as[Long], a(1).as[Long]))
-    }
-  }
-
   implicit val cacheFormat: Format[Map[String, (Price, Price)]] = new Format[Map[String, (Long, Long)]] {
     def writes(cache: Map[String, (Long, Long)]): JsValue =
       JsObject(cache.mapValues(v => Json.arr(v._1, v._2)))
@@ -161,4 +147,54 @@ object EventSerializers {
 
   implicit val snapshotFormat: Format[Snapshot] =
     Format((JsPath \ "o").read[OrderBook].map(Snapshot), Writes[Snapshot](s => Json.obj("o" -> s.orderBook)))
+
+  private def encodeOrder(o: LimitOrder) = {
+    val orderBytes = 1.toByte +: o.order.bytes() // todo: change this to support orders v2+
+    ByteBuffer
+      .allocate(orderBytes.length + 16)
+      .putLong(o.amount)
+      .putLong(o.fee)
+      .put(orderBytes)
+      .array()
+  }
+
+  private def decodeOrder(bytes: Array[Byte]) = {
+    val buf        = ByteBuffer.wrap(bytes)
+    val amount     = buf.getLong
+    val fee        = buf.getLong
+    val orderBytes = bytes.drop(16)
+    val order = orderBytes.head match {
+      // todo: support orders v2+
+      case 1 => Order.parseBytes(orderBytes.tail).get
+    }
+    LimitOrder.limitOrder(amount, fee, order)
+  }
+
+  private def encodeOrderAdded(oa: OrderAdded)     = encodeOrder(oa.order)
+  private def decodeOrderAdded(bytes: Array[Byte]) = OrderAdded(decodeOrder(bytes))
+
+  private def encodeOrderExecuted(oe: OrderExecuted) = {
+    val submittedBytes = encodeOrder(oe.submitted)
+    val counterBytes   = encodeOrder(oe.counter)
+
+    ByteBuffer
+      .allocate(8 + submittedBytes.length + counterBytes.length)
+      .putInt(submittedBytes.length)
+      .put(submittedBytes)
+      .putInt(counterBytes.length)
+      .put(counterBytes)
+      .array()
+  }
+
+  private def decodeOrderExecuted(bytes: Array[Byte]) = {
+    val bb        = ByteBuffer.wrap(bytes)
+    val submitted = new Array[Byte](bb.getInt)
+    bb.get(submitted)
+    val counter = new Array[Byte](bb.getInt)
+    bb.get(counter)
+    OrderExecuted(decodeOrder(submitted), decodeOrder(counter))
+  }
+
+  private def encodeOrderCancelled(oc: OrderCanceled)  = (if (oc.unmatchable) 1 else 0).toByte +: encodeOrder(oc.limitOrder)
+  private def decodeOrderCancelled(bytes: Array[Byte]) = OrderCanceled(decodeOrder(bytes.tail), bytes.head == 1)
 }