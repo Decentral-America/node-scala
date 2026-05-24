930ae10d583eda224f31f7e788edbbef2f49bf62
jourlez <josuecr.288@gmail.com>
2026-05-24 06:26:52 -0600

    fix(deps): correct curve25519-java artifactId → curve25519 in node-scala Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
   val machinist       = "org.typelevel"      %% "machinist"         % "0.6.8"
   val logback         = "ch.qos.logback"      % "logback-classic"   % "1.5.32"
   val asyncHttpClient = "org.asynchttpclient" % "async-http-client" % "3.0.10"
-  val curve25519      = "io.decentralchain"  % "curve25519-java"   % "0.6.6"
+  val curve25519      = "io.decentralchain"  % "curve25519"        % "0.6.6"
   val nettyHandler    = nettyModule("handler")
 
   val playJson = "org.playframework" %% "play-json" % "3.0.6"
