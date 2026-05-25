27971447a6f1c8cde7115ba6841f1f187f969149
jourlez <josuecr.288@gmail.com>
2026-05-25 12:16:02 -0600

    deps: bump okio-jvm 3.16.4 → 3.17.0 (Audit 41)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     Seq(
       "org.scala-lang"           %% "scala3-library" % scalaVersion.value,
       "com.google.code.gson"      % "gson"           % "2.14.0",
-      "com.squareup.okio"         % "okio-jvm"       % "3.16.4",
+      "com.squareup.okio"         % "okio-jvm"       % "3.17.0",
       "org.apache.httpcomponents" % "httpclient"     % "4.5.14",
       "org.slf4j"                 % "slf4j-api"      % "2.0.18",
       "org.msgpack"               % "msgpack-core"   % "0.9.12",
