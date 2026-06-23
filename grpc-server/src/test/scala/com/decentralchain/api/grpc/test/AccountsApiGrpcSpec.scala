hearthchain/node-jvm

OpenAPI and improvements (#4034)
* OpenAPI:
- /transactions/sign: added an info about new fields to description
- New components: BlsPublicKey, BlsSignature, GeneratorIndex, BlockEndorsement, FinalizationVoting
- Added finalizationVoting to block headers

* Better logs and more filters for endorsements (exclusion of miner endorsements)

* OpenAPI: Added CommitToGenerationTransaction

* OpenAPI: added /addresses/bls/{address}

* OpenAPI, added:
- GET /blocks/height/finalized
- GET /blocks/headers/finalized
- GET /blocks/finalized/at/{height}

* OpenAPI: New fields "nextCommittedGeneratorsHash" and "committedGeneratorBalancesHash" in  `/debug/stateHash/last` and `/debug/stateHash/{height}` after finality feature activation

* OpenAPI: new endpoint `/generators/at/{height}`

* OpenAPI: new field nextCommittedGenerators in TransactionSnapshot

* OpenAPI: only few transaction types has `feeAssetId`: InvokeExpressionTransaction, InvokeScriptTransaction, TransferTransaction, UpdateAssetInfoTransaction

* Updated tests

* Forced rollback fix

* Updated Swagger UI to 5.31.1

* Swagger UI: enable "Try it" mode always, hide "Try it out" button

* Swagger UI: Disable "Explore" functionality

* Swagger UI: Disable choosing other server

* OpenAPI: transaction schema changes to help Swagger UI show precise types instead of "object"

* OpenAPI:
- Added WavesOrAssetId and DebugStateHashes schemas
- Eliminate deprecated "nullable"
- Right types for assetId-like fields
- Chores

* OpenAPI: Added AssetPair

* DB Keys: ported finalizedHeight parsing

* Open API: Fixes

* Removed obsolete code

* New balance tests, common error format WIP

* New balance tests, common error format WIP (2)

* HTTP API: TransactionId and BlockId matchers blocked route fallback
by throwing

* CommitToGenerationTransaction: precise balance validation

* Fixed a keyblock validation against the latest removed microblock

* New test for appending after removing the last microblock

* Added comments

* Fixed tes

grpc-server/src/test/scala/com/wavesplatform/api/grpc/test/AccountsApiGrpcSpec.scala
@@ -4,7 +4,6 @@ import com.google.protobuf.ByteString
 import com.wavesplatform.TestValues
 import com.wavesplatform.account.{Address, KeyPair}
 import com.wavesplatform.api.grpc.*
-import com.wavesplatform.block.Block
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.crypto.DigestLength
 import com.wavesplatform.db.WithDomain
@@ -12,15 +11,15 @@ import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.history.Domain
 import com.wavesplatform.protobuf.Amount
 import com.wavesplatform.protobuf.transaction.{DataEntry, Recipient}
-import com.wavesplatform.state.{Height, BlockRewardCalculator, EmptyDataEntry, IntegerDataEntry}
+import com.wavesplatform.state.{BlockRewardCalculator, EmptyDataEntry, Height, IntegerDataEntry}
 import com.wavesplatform.test.*
+import com.wavesplatform.test.DomainPresets.*
 import com.wavesplatform.transaction.Asset.Waves
 import com.wavesplatform.transaction.TxHelpers
 import com.wavesplatform.utils.{DiffMatchers, Schedulers}
-import org.scalatest.{Assertion, BeforeAndAfterAll}
-import com.wavesplatform.test.DomainPresets.*
 import monix.execution.ExecutionModel.SynchronousExecution
 import monix.execution.Scheduler
+import org.scalatest.{Assertion, BeforeAndAfterAll}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
@@ -189,7 +188,10 @@ class AccountsApiGrpcSpec extends FreeSpec with BeforeAndAfterAll with DiffMatch
 
     val sender          = TxHelpers.signer(1)
     val challengedMiner = TxHelpers.signer(2)
-    withDomain(TransactionStateSnapshot.configure(_.copy(lightNodeBlockFieldsAbsenceInterval = 0)), balances = AddrWithBalance.enoughBalances(sender)) { d =>
+    withDomain(
+      TransactionStateSnapshot.configure(_.copy(lightNodeBlockFieldsAbsenceInterval = 0)),
+      balances = AddrWithBalance.enoughBalances(sender)
+    ) { d =>
       val grpcApi = getGrpcApi(d)
 
       val challengingMiner = d.wallet.generateNewAccount().get
@@ -206,8 +208,6 @@ class AccountsApiGrpcSpec extends FreeSpec with BeforeAndAfterAll with DiffMatch
 
       val invalidStateHash = ByteStr.fill(DigestLength)(1)
       val originalBlock = d.createBlock(
-        Block.ProtoBlockVersion,
-        Seq.empty,
         strictTime = true,
         generator = challengedMiner,
         stateHash = Some(Some(invalidStateHash))

node/src/main/scala/com/wavesplatform/api/http/GeneratorsApiRoute.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.api.http
 
 import com.wavesplatform.api.common.CommonGeneratorsApi
+import com.wavesplatform.api.http.GeneratorsApiRoute.mkGeneratorEntryJson
 import com.wavesplatform.settings.RestAPISettings
 import com.wavesplatform.state.{Blockchain, Height}
 import com.wavesplatform.utils.Time
@@ -8,37 +9,46 @@ import monix.eval.Task
 import org.apache.pekko.http.scaladsl.model.StatusCodes
 import org.apache.pekko.http.scaladsl.model.headers.Accept
 import org.apache.pekko.http.scaladsl.server.Route
-import play.api.libs.json.{JsNumber, JsString, Json}
+import play.api.libs.json.{JsNumber, JsObject, JsString, Json}
 
 case class GeneratorsApiRoute(settings: RestAPISettings, blockchain: Blockchain, api: CommonGeneratorsApi, time: Time, routeTimeout: RouteTimeout)
     extends ApiRoute {
   override lazy val route: Route = (path("generators" / "at" / IntNumber) & get & optionalHeaderValueByType(Accept)) { (height, accept) =>
-    if (height > blockchain.height) complete(StatusCodes.NotFound, Json.arr())
-    else
-      routeTimeout.executeToFuture {
-        Task {
-          val formatNumbersAsStrings = accept.fold(false) {
-            case a if a.mediaRanges.exists(CustomJson.acceptsNumbersAsStrings) => true
-            case _                                                             => false
-          }
-
-          api.generators(Height(height)).map { x =>
-            val balance = if (formatNumbersAsStrings) JsString(x.balance.toString) else JsNumber(x.balance)
-
-            val builder = Json.newBuilder
-            builder ++= Seq(
-              "address"       -> x.address.toString,
-              "balance"       -> balance,
-              "transactionId" -> x.commitTxnId.toString
-            )
-
-            x.conflictHeight.foreach { h =>
-              builder += "conflictHeight" -> h.toInt
+    (blockchain.currentGenerationPeriod, blockchain.generationPeriodOf(Height(height))) match {
+      case (Some(currGenerationPeriod), Some(reqGenerationPeriod)) if reqGenerationPeriod <= currGenerationPeriod.next =>
+        routeTimeout.executeToFuture {
+          Task {
+            val formatNumbersAsStrings = accept.fold(false) {
+              case a if a.mediaRanges.exists(CustomJson.acceptsNumbersAsStrings) => true
+              case _                                                             => false
             }
 
-            builder.result()
+            api.generators(Height(height)).map(mkGeneratorEntryJson(_, formatNumbersAsStrings))
           }
         }
-      }
+
+      case _ => complete(StatusCodes.NotFound, Json.arr())
+    }
+  }
+}
+
+object GeneratorsApiRoute {
+  def mkGeneratorEntryJson(x: CommonGeneratorsApi.GeneratorEntry, formatNumbersAsStrings: Boolean): JsObject = {
+    val builder = Json.newBuilder
+    builder ++= Seq(
+      "address"       -> x.address.toString,
+      "transactionId" -> x.commitTxnId.toString
+    )
+
+    x.balance.foreach { b =>
+      val v = if (formatNumbersAsStrings) JsString(b.toString) else JsNumber(b)
+      builder += "balance" -> v
+    }
+
+    x.conflictHeight.foreach { h =>
+      builder += "conflictHeight" -> h.toInt
+    }
+
+    builder.result()
   }
 }

node/tests/src/test/scala/com/wavesplatform/finalization/conflict/ConflictEndorserRecommitmentSuite.scala
@@ -1,18 +1,14 @@
 package com.wavesplatform.finalization.conflict
 
 import com.wavesplatform.TestValues
-import com.wavesplatform.block.Block
 import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.finalization.BaseFinalizationSpec
 import com.wavesplatform.state.diffs.ENOUGH_AMT
-import com.wavesplatform.state.{Blockchain, GeneratorIndex, Height, Portfolio}
+import com.wavesplatform.state.{GeneratorIndex, Height, Portfolio}
 import com.wavesplatform.test.DomainPresets.WavesSettingsOps
-import com.wavesplatform.test.FreeSpec
 import com.wavesplatform.transaction.CommitToGenerationTransaction.DepositInWavelets
 import com.wavesplatform.transaction.TxHelpers
-import org.scalactic.source.Position
-import org.scalatest.Assertion
 
 class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
   private val validGenerator = TxHelpers.signer(0)
@@ -34,7 +30,7 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
   "punished and committed to next" in withDomain(defaultSettings, AddrWithBalance.enoughBalances(validGenerator, conflictGenerator)) { d =>
     log.debug(s"Append block 2 with commitments")
     val block2Txs             = generators.map(x => TxHelpers.commitToGeneration(generationPeriodStart = Height(3), x))
-    val block2WithCommitments = d.createBlock(version = Block.ProtoBlockVersion, txs = block2Txs, generator = validGenerator, strictTime = true)
+    val block2WithCommitments = d.createBlock(block2Txs, generator = validGenerator, strictTime = true)
     d.appender.appendBlock(block2WithCommitments)
 
     val balanceAfter1 = ENOUGH_AMT
@@ -42,8 +38,6 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
 
     log.debug(s"Append block 3 with votes")
     val block3WithVotes = d.createBlock(
-      version = Block.ProtoBlockVersion,
-      txs = Nil,
       generator = validGenerator,
       strictTime = true,
       finalizationVoting = Some(mkFinalizationVoting().withConflict(conflictGenerator, GeneratorIndex(1), block2WithCommitments.id()))
@@ -52,13 +46,13 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
 
     log.debug(s"Append block 3 with punishment and commitment")
     val block4Txs             = generators.map(x => TxHelpers.commitToGeneration(generationPeriodStart = Height(5), x))
-    val block4WithCommitments = d.createBlock(version = Block.ProtoBlockVersion, txs = block4Txs, generator = validGenerator, strictTime = true)
+    val block4WithCommitments = d.createBlock(block4Txs, generator = validGenerator, strictTime = true)
     d.appender.appendBlock(block4WithCommitments)
 
     val balanceAfter4 = ENOUGH_AMT - 2 * TestValues.commitToGenerationFee - DepositInWavelets
 
     log.debug("Append block 5 of new period")
-    d.appender.appendBlock(d.createBlock(version = Block.ProtoBlockVersion, txs = Nil, generator = validGenerator, strictTime = true))
+    d.appender.appendBlock(d.createBlock(generator = validGenerator, strictTime = true))
 
     withClue(s"checkCommitted: ") {
       d.blockchain.committedGenerators(d.blockchain.currentGenerationPeriod.value).map(_._1) should contain theSameElementsInOrderAs generatorAddrs
@@ -75,7 +69,7 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
       d.generatorsApi
         .generators(Height(d.blockchain.height))
         .collectFirst { case x if x.address == conflictGeneratorAddr => x.balance }
-        .getOrElse(0L) shouldBe balanceAfter4 - DepositInWavelets
+        .value shouldBe Some(balanceAfter4 - DepositInWavelets)
     }
 
     d.blockchain.balanceSnapshots(conflictGeneratorAddr, from = 2, to = None) should contain theSameElementsInOrderAs Seq(

Decentral-America/node-scala

grpc-server/src/test/scala/com/decentralchain/api/grpc/test/AccountsApiGrpcSpec.scala
@@ -4,24 +4,22 @@ import com.google.protobuf.ByteString
 import com.decentralchain.TestValues
 import com.decentralchain.account.{Address, KeyPair}
 import com.decentralchain.api.grpc.*
-import io.decentralchain.api.grpc.*
-import com.decentralchain.block.Block
 import com.decentralchain.common.state.ByteStr
 import com.decentralchain.crypto.DigestLength
 import com.decentralchain.db.WithDomain
 import com.decentralchain.db.WithState.AddrWithBalance
 import com.decentralchain.history.Domain
-import io.decentralchain.protobuf.Amount
-import io.decentralchain.protobuf.transaction.{DataEntry, Recipient}
-import com.decentralchain.state.{Height, BlockRewardCalculator, EmptyDataEntry, IntegerDataEntry}
+import com.decentralchain.protobuf.Amount
+import com.decentralchain.protobuf.transaction.{DataEntry, Recipient}
+import com.decentralchain.state.{BlockRewardCalculator, EmptyDataEntry, Height, IntegerDataEntry}
 import com.decentralchain.test.*
-import com.decentralchain.transaction.Asset.Dcc
+import com.decentralchain.test.DomainPresets.*
+import com.decentralchain.transaction.Asset.Waves
 import com.decentralchain.transaction.TxHelpers
 import com.decentralchain.utils.{DiffMatchers, Schedulers}
-import org.scalatest.{Assertion, BeforeAndAfterAll}
-import com.decentralchain.test.DomainPresets.*
 import monix.execution.ExecutionModel.SynchronousExecution
 import monix.execution.Scheduler
+import org.scalatest.{Assertion, BeforeAndAfterAll}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
@@ -210,8 +208,6 @@ class AccountsApiGrpcSpec extends FreeSpec with BeforeAndAfterAll with DiffMatch
 
       val invalidStateHash = ByteStr.fill(DigestLength)(1)
       val originalBlock = d.createBlock(
-        Block.ProtoBlockVersion,
-        Seq.empty,
         strictTime = true,
         generator = challengedMiner,
         stateHash = Some(Some(invalidStateHash))

node/src/main/scala/com/decentralchain/api/http/GeneratorsApiRoute.scala
@@ -1,6 +1,7 @@
 package com.decentralchain.api.http
 
 import com.decentralchain.api.common.CommonGeneratorsApi
+import com.decentralchain.api.http.GeneratorsApiRoute.mkGeneratorEntryJson
 import com.decentralchain.settings.RestAPISettings
 import com.decentralchain.state.{Blockchain, Height}
 import com.decentralchain.utils.Time
@@ -8,37 +9,46 @@ import monix.eval.Task
 import org.apache.pekko.http.scaladsl.model.StatusCodes
 import org.apache.pekko.http.scaladsl.model.headers.Accept
 import org.apache.pekko.http.scaladsl.server.Route
-import play.api.libs.json.{JsNumber, JsString, Json}
+import play.api.libs.json.{JsNumber, JsObject, JsString, Json}
 
 case class GeneratorsApiRoute(settings: RestAPISettings, blockchain: Blockchain, api: CommonGeneratorsApi, time: Time, routeTimeout: RouteTimeout)
     extends ApiRoute {
   override lazy val route: Route = (path("generators" / "at" / IntNumber) & get & optionalHeaderValueByType(Accept)) { (height, accept) =>
-    if (height > blockchain.height) complete(StatusCodes.NotFound, Json.arr())
-    else
-      routeTimeout.executeToFuture {
-        Task {
-          val formatNumbersAsStrings = accept.fold(false) {
-            case a if a.mediaRanges.exists(CustomJson.acceptsNumbersAsStrings) => true
-            case _                                                             => false
-          }
-
-          api.generators(Height(height)).map { x =>
-            val balance = if (formatNumbersAsStrings) JsString(x.balance.toString) else JsNumber(x.balance)
-
-            val builder = Json.newBuilder
-            builder ++= Seq(
-              "address"       -> x.address.toString,
-              "balance"       -> balance,
-              "transactionId" -> x.commitTxnId.toString
-            )
-
-            x.conflictHeight.foreach { h =>
-              builder += "conflictHeight" -> h.toInt
+    (blockchain.currentGenerationPeriod, blockchain.generationPeriodOf(Height(height))) match {
+      case (Some(currGenerationPeriod), Some(reqGenerationPeriod)) if reqGenerationPeriod <= currGenerationPeriod.next =>
+        routeTimeout.executeToFuture {
+          Task {
+            val formatNumbersAsStrings = accept.fold(false) {
+              case a if a.mediaRanges.exists(CustomJson.acceptsNumbersAsStrings) => true
+              case _                                                             => false
             }
 
-            builder.result()
+            api.generators(Height(height)).map(mkGeneratorEntryJson(_, formatNumbersAsStrings))
           }
         }
-      }
+
+      case _ => complete(StatusCodes.NotFound, Json.arr())
+    }
+  }
+}
+
+object GeneratorsApiRoute {
+  def mkGeneratorEntryJson(x: CommonGeneratorsApi.GeneratorEntry, formatNumbersAsStrings: Boolean): JsObject = {
+    val builder = Json.newBuilder
+    builder ++= Seq(
+      "address"       -> x.address.toString,
+      "transactionId" -> x.commitTxnId.toString
+    )
+
+    x.balance.foreach { b =>
+      val v = if (formatNumbersAsStrings) JsString(b.toString) else JsNumber(b)
+      builder += "balance" -> v
+    }
+
+    x.conflictHeight.foreach { h =>
+      builder += "conflictHeight" -> h.toInt
+    }
+
+    builder.result()
   }
 }

node/tests/src/test/scala/com/decentralchain/finalization/conflict/ConflictEndorserRecommitmentSuite.scala
@@ -1,18 +1,14 @@
 package com.decentralchain.finalization.conflict
 
 import com.decentralchain.TestValues
-import com.decentralchain.block.Block
 import com.decentralchain.db.WithState.AddrWithBalance
 import com.decentralchain.features.BlockchainFeatures
 import com.decentralchain.finalization.BaseFinalizationSpec
 import com.decentralchain.state.diffs.ENOUGH_AMT
-import com.decentralchain.state.{Blockchain, GeneratorIndex, Height, Portfolio}
-import com.decentralchain.test.DomainPresets.DCCSettingsOps
-import com.decentralchain.test.FreeSpec
-import com.decentralchain.transaction.CommitToGenerationTransaction.DepositInDcclets
+import com.decentralchain.state.{GeneratorIndex, Height, Portfolio}
+import com.decentralchain.test.DomainPresets.WavesSettingsOps
+import com.decentralchain.transaction.CommitToGenerationTransaction.DepositInWavelets
 import com.decentralchain.transaction.TxHelpers
-import org.scalactic.source.Position
-import org.scalatest.Assertion
 
 class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
   private val validGenerator = TxHelpers.signer(0)
@@ -34,7 +30,7 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
   "punished and committed to next" in withDomain(defaultSettings, AddrWithBalance.enoughBalances(validGenerator, conflictGenerator)) { d =>
     log.debug(s"Append block 2 with commitments")
     val block2Txs             = generators.map(x => TxHelpers.commitToGeneration(generationPeriodStart = Height(3), x))
-    val block2WithCommitments = d.createBlock(version = Block.ProtoBlockVersion, txs = block2Txs, generator = validGenerator, strictTime = true)
+    val block2WithCommitments = d.createBlock(block2Txs, generator = validGenerator, strictTime = true)
     d.appender.appendBlock(block2WithCommitments)
 
     val balanceAfter1 = ENOUGH_AMT
@@ -42,8 +38,6 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
 
     log.debug(s"Append block 3 with votes")
     val block3WithVotes = d.createBlock(
-      version = Block.ProtoBlockVersion,
-      txs = Nil,
       generator = validGenerator,
       strictTime = true,
       finalizationVoting = Some(mkFinalizationVoting().withConflict(conflictGenerator, GeneratorIndex(1), block2WithCommitments.id()))
@@ -52,13 +46,13 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
 
     log.debug(s"Append block 3 with punishment and commitment")
     val block4Txs             = generators.map(x => TxHelpers.commitToGeneration(generationPeriodStart = Height(5), x))
-    val block4WithCommitments = d.createBlock(version = Block.ProtoBlockVersion, txs = block4Txs, generator = validGenerator, strictTime = true)
+    val block4WithCommitments = d.createBlock(block4Txs, generator = validGenerator, strictTime = true)
     d.appender.appendBlock(block4WithCommitments)
 
     val balanceAfter4 = ENOUGH_AMT - 2 * TestValues.commitToGenerationFee - DepositInDcclets
 
     log.debug("Append block 5 of new period")
-    d.appender.appendBlock(d.createBlock(version = Block.ProtoBlockVersion, txs = Nil, generator = validGenerator, strictTime = true))
+    d.appender.appendBlock(d.createBlock(generator = validGenerator, strictTime = true))
 
     withClue(s"checkCommitted: ") {
       d.blockchain.committedGenerators(d.blockchain.currentGenerationPeriod.value).map(_._1) should contain theSameElementsInOrderAs generatorAddrs
@@ -75,7 +69,7 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
       d.generatorsApi
         .generators(Height(d.blockchain.height))
         .collectFirst { case x if x.address == conflictGeneratorAddr => x.balance }
-        .getOrElse(0L) shouldBe balanceAfter4 - DepositInDcclets
+        .value shouldBe Some(balanceAfter4 - DepositInWavelets)
     }
 
     d.blockchain.balanceSnapshots(conflictGeneratorAddr, from = 2, to = None) should contain theSameElementsInOrderAs Seq(