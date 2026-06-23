hearthchain/node-jvm

BLST smoke test & improved tests (#4037)

node-generator/src/main/scala/com/wavesplatform/generator/SmartGenerator.scala
@@ -9,10 +9,10 @@ import com.wavesplatform.generator.utils.Implicits.DoubleExt
 import com.wavesplatform.lang.script.Script
 import com.wavesplatform.lang.v1.estimator.ScriptEstimator
 import com.wavesplatform.transaction.Asset.Waves
-import com.wavesplatform.transaction.assets.exchange.{AssetPair, ExchangeTransaction, Order}
+import com.wavesplatform.transaction.assets.exchange.{AssetPair, Order}
 import com.wavesplatform.transaction.smart.SetScriptTransaction
 import com.wavesplatform.transaction.transfer.TransferTransaction
-import com.wavesplatform.transaction.{Asset, Transaction, TxVersion}
+import com.wavesplatform.transaction.{Asset, Proofs, Transaction, TxHelpers, TxVersion}
 import pureconfig.ConfigReader
 
 import java.util.concurrent.ThreadLocalRandom
@@ -33,14 +33,18 @@ class SmartGenerator(settings: SmartGenerator.Settings, val accounts: Seq[KeyPai
 
     val setScripts = Range(0, settings.scripts) flatMap (_ =>
       accounts.map { i =>
-        SetScriptTransaction.selfSigned(1.toByte, i, Some(script), 1.waves, System.currentTimeMillis()).explicitGet()
+        SetScriptTransaction
+          .create(1.toByte, i.publicKey, Some(script), 1.waves, System.currentTimeMillis(), Proofs.empty)
+          .map(_.signWith(i.privateKey))
+          .explicitGet()
       }
     )
 
     val now = System.currentTimeMillis()
     val txs = Range(0, settings.transfers).map { i =>
       TransferTransaction
-        .selfSigned(2.toByte, bank, bank.toAddress, Waves, 1.waves - 2 * fee, Waves, fee, ByteStr.empty, now + i)
+        .create(2.toByte, bank.publicKey, bank.toAddress, Waves, 1.waves - 2 * fee, Waves, fee, ByteStr.empty, now + i, Proofs.empty)
+        .map(_.signWith(bank.privateKey))
         .explicitGet()
     }
 
@@ -56,9 +60,7 @@ class SmartGenerator(settings: SmartGenerator.Settings, val accounts: Seq[KeyPai
       val sellOrder = Order.sell(TxVersion.V2, seller, matcher.publicKey, pair, 100000000L, 1, ts, ts + 30.days.toMillis, 0.003.waves).explicitGet()
       val buyOrder  = Order.buy(TxVersion.V2, buyer, matcher.publicKey, pair, 100000000L, 1, ts, ts + 1.day.toMillis, 0.003.waves).explicitGet()
 
-      ExchangeTransaction
-        .signed(TxVersion.V2, matcher.privateKey, buyOrder, sellOrder, 100000000, 1, 0.003.waves, 0.003.waves, 0.011.waves, ts)
-        .explicitGet()
+      TxHelpers.exchange(buyOrder, sellOrder, matcher, 100000000, 1, 0.003.waves, 0.003.waves, 0.011.waves, ts)
     }
 
     setScripts ++ txs ++ extxs

node-it/src/test/scala/com/wavesplatform/it/sync/smartcontract/Ride4DAppsActivationTestSuite.scala
@@ -1,18 +1,19 @@
 package com.wavesplatform.it.sync.smartcontract
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.Config
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2.*
+import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.it.NodeConfigs
 import com.wavesplatform.it.api.SyncHttpApi.*
 import com.wavesplatform.it.sync.*
 import com.wavesplatform.it.transactions.BaseTransactionSuite
 import com.wavesplatform.lang.v1.estimator.v2.ScriptEstimatorV2
-import com.wavesplatform.test.*
-import com.wavesplatform.transaction.{Asset, AssetIdLength}
-import com.wavesplatform.transaction.smart.script.ScriptCompiler
-import org.scalatest.CancelAfterFailure
 import com.wavesplatform.state.Height
+import com.wavesplatform.test.*
+import com.wavesplatform.transaction.smart.script.ScriptCompiler
+import com.wavesplatform.transaction.{Asset, AssetIdLength}
+import org.scalatest.CancelAfterFailure
 
 import scala.concurrent.duration.*
 
@@ -182,18 +183,6 @@ class Ride4DAppsActivationTestSuite extends BaseTransactionSuite with CancelAfte
 object Ride4DAppsActivationTestSuite {
   val activationHeight = Height(15)
 
-  val configWithRide4DAppsFeature = NodeConfigs.newBuilder
-    .withDefault(1)
-    .withSpecial(1, _.nonMiner)
-    .buildNonConflicting()
-    .map(
-      ConfigFactory
-        .parseString(
-          s"""waves.blockchain.custom.functionality {
-             |  pre-activated-features.11 = ${activationHeight - 1}
-             |}""".stripMargin
-        )
-        .withFallback(_)
-    )
-
+  import NodeConfigs.*
+  val configWithRide4DAppsFeature = Seq(BiggestMiner, NotMiner).map(_.preactivatedFeatures((BlockchainFeatures.Ride4DApps, activationHeight - 1)))
 }

node-it/src/test/scala/com/wavesplatform/it/sync/transactions/UpdateAssetInfoTransactionSuite.scala
@@ -15,6 +15,7 @@ import com.wavesplatform.lang.directives.values.V4
 import com.wavesplatform.lang.v1.compiler.{Terms, TestCompiler}
 import com.wavesplatform.lang.v1.estimator.v3.ScriptEstimatorV3
 import com.wavesplatform.state.Height
+import com.wavesplatform.transaction.Asset.IssuedAsset
 import com.wavesplatform.transaction.assets.IssueTransaction.{MaxAssetDescriptionLength, MaxAssetNameLength, MinAssetNameLength}
 import com.wavesplatform.transaction.smart.script.ScriptCompiler
 import com.wavesplatform.transaction.{TransactionType, TxVersion}
@@ -118,9 +119,8 @@ class UpdateAssetInfoTransactionSuite extends BaseTransactionSuite with CancelAf
         UpdateAssetInfoRequest(
           TxVersion.V1,
           AddressScheme.current.chainId,
-          None,
-          Some(issuer.publicKey.toString),
-          assetId,
+          issuer.publicKey.toString,
+          IssuedAsset(ByteStr(new Array[Byte](32))),
           "test",
           "test",
           None,

node/src/main/scala/com/wavesplatform/transaction/assets/ReissueTransaction.scala
@@ -1,10 +1,11 @@
 package com.wavesplatform.transaction.assets
 
-import com.wavesplatform.account.{AddressScheme, KeyPair, PrivateKey, PublicKey}
+import com.wavesplatform.account.{AddressScheme, PrivateKey, PublicKey}
+import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.crypto
 import com.wavesplatform.lang.ValidationError
-import com.wavesplatform.transaction.Asset.IssuedAsset
 import com.wavesplatform.transaction.*
+import com.wavesplatform.transaction.Asset.IssuedAsset
 import com.wavesplatform.transaction.serialization.impl.ReissueTxSerializer
 import com.wavesplatform.transaction.validation.TxValidator
 import com.wavesplatform.transaction.validation.impl.ReissueTxValidator
@@ -26,14 +27,17 @@ case class ReissueTransaction(
 ) extends Transaction(TransactionType.Reissue, Seq(asset))
     with Versioned.ToV3
     with ProvenTransaction
-    with SigProofsSwitch
+    with HasSignature
     with TxWithFee.InWaves
     with FastHashId
     with PBSince.V3 {
+  override type T = ReissueTransaction
 
   override val bodyBytes: Coeval[Array[Byte]] = Coeval.evalOnce(ReissueTxSerializer.bodyBytes(this))
   override val bytes: Coeval[Array[Byte]]     = Coeval.evalOnce(ReissueTxSerializer.toBytes(this))
   override val json: Coeval[JsObject]         = Coeval.evalOnce(ReissueTxSerializer.toJson(this))
+
+  override def addProof(proof: ByteStr): ReissueTransaction = copy(proofs = this.proofs.add(proof))
 }
 
 object ReissueTransaction extends TransactionParser {
@@ -56,7 +60,7 @@ object ReissueTransaction extends TransactionParser {
       reissuable: Boolean,
       fee: Long,
       timestamp: Long,
-      proofs: Proofs,
+      proofs: Proofs = Proofs.empty,
       chainId: Byte = AddressScheme.current.chainId
   ): Either[ValidationError, ReissueTransaction] =
     for {
@@ -64,29 +68,4 @@ object ReissueTransaction extends TransactionParser {
       quantity <- TxPositiveAmount(quantity)(TxValidationError.NonPositiveAmount(quantity, "assets"))
       tx       <- ReissueTransaction(version, sender, asset, quantity, reissuable, fee, timestamp, proofs, chainId).validatedEither
     } yield tx
-
-  def signed(
-      version: TxVersion,
-      sender: PublicKey,
-      asset: IssuedAsset,
-      quantity: Long,
-      reissuable: Boolean,
-      fee: Long,
-      timestamp: Long,
-      signer: PrivateKey,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ReissueTransaction] =
-    create(version, sender, asset, quantity, reissuable, fee, timestamp, Nil, chainId).map(_.signWith(signer))
-
-  def selfSigned(
-      version: TxVersion,
-      sender: KeyPair,
-      asset: IssuedAsset,
-      quantity: Long,
-      reissuable: Boolean,
-      fee: Long,
-      timestamp: Long,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ReissueTransaction] =
-    signed(version, sender.publicKey, asset, quantity, reissuable, fee, timestamp, sender.privateKey, chainId)
 }

node/src/main/scala/com/wavesplatform/transaction/assets/exchange/ExchangeTransaction.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.transaction.assets.exchange
 
 import com.wavesplatform.account.{AddressScheme, PrivateKey, PublicKey}
+import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.crypto
 import com.wavesplatform.lang.ValidationError
 import com.wavesplatform.transaction.*
@@ -26,13 +27,17 @@ case class ExchangeTransaction(
     timestamp: Long,
     proofs: Proofs,
     chainId: Byte
-) extends Transaction(TransactionType.Exchange, order1.assetPair.checkedAssets)
-    with Versioned.ToV3
-    with ProvenTransaction
-    with TxWithFee.InWaves
-    with FastHashId
-    with SigProofsSwitch
-    with PBSince.V3 {
+) extends Transaction(TransactionType.Exchange, order1.assetPair.checkedAssets),
+      ProvenTransaction,
+      HasSignature,
+      Versioned.ToV3,
+      TxWithFee.InWaves,
+      FastHashId,
+      PBSince.V3 {
+
+  override type T = ExchangeTransaction
+
+  override def addProof(proof: ByteStr): ExchangeTransaction = copy(proofs = proofs.add(proof))
 
   val (buyOrder, sellOrder) = if (order1.orderType == OrderType.BUY) (order1, order2) else (order2, order1)
 
@@ -98,19 +103,4 @@ object ExchangeTransaction extends TransactionParser {
         chainId
       ).validatedEither
     } yield tx
-
-  def signed(
-      version: TxVersion,
-      matcher: PrivateKey,
-      order1: Order,
-      order2: Order,
-      amount: Long,
-      price: Long,
-      buyMatcherFee: Long,
-      sellMatcherFee: Long,
-      fee: Long,
-      timestamp: Long,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ExchangeTransaction] =
-    create(version, order1, order2, amount, price, buyMatcherFee, sellMatcherFee, fee, timestamp, Proofs.empty, chainId).map(_.signWith(matcher))
 }

Decentral-America/node-scala

node-generator/src/main/scala/com/decentralchain/generator/SmartGenerator.scala
@@ -8,11 +8,11 @@ import com.decentralchain.generator.utils.Gen
 import com.decentralchain.generator.utils.Implicits.DoubleExt
 import com.decentralchain.lang.script.Script
 import com.decentralchain.lang.v1.estimator.ScriptEstimator
-import com.decentralchain.transaction.Asset.Dcc
-import com.decentralchain.transaction.assets.exchange.{AssetPair, ExchangeTransaction, Order}
+import com.decentralchain.transaction.Asset.Waves
+import com.decentralchain.transaction.assets.exchange.{AssetPair, Order}
 import com.decentralchain.transaction.smart.SetScriptTransaction
 import com.decentralchain.transaction.transfer.TransferTransaction
-import com.decentralchain.transaction.{Asset, Transaction, TxVersion}
+import com.decentralchain.transaction.{Asset, Proofs, Transaction, TxHelpers, TxVersion}
 import pureconfig.ConfigReader
 
 import java.util.concurrent.ThreadLocalRandom
@@ -33,14 +33,18 @@ class SmartGenerator(settings: SmartGenerator.Settings, val accounts: Seq[KeyPai
 
     val setScripts = Range(0, settings.scripts) flatMap (_ =>
       accounts.map { i =>
-        SetScriptTransaction.selfSigned(1.toByte, i, Some(script), 1.dcc, System.currentTimeMillis()).explicitGet()
+        SetScriptTransaction
+          .create(1.toByte, i.publicKey, Some(script), 1.waves, System.currentTimeMillis(), Proofs.empty)
+          .map(_.signWith(i.privateKey))
+          .explicitGet()
       }
     )
 
     val now = System.currentTimeMillis()
     val txs = Range(0, settings.transfers).map { i =>
       TransferTransaction
-        .selfSigned(2.toByte, bank, bank.toAddress, Dcc, 1.dcc - 2 * fee, Dcc, fee, ByteStr.empty, now + i)
+        .create(2.toByte, bank.publicKey, bank.toAddress, Waves, 1.waves - 2 * fee, Waves, fee, ByteStr.empty, now + i, Proofs.empty)
+        .map(_.signWith(bank.privateKey))
         .explicitGet()
     }
 
@@ -56,9 +60,7 @@ class SmartGenerator(settings: SmartGenerator.Settings, val accounts: Seq[KeyPai
       val sellOrder = Order.sell(TxVersion.V2, seller, matcher.publicKey, pair, 100000000L, 1, ts, ts + 30.days.toMillis, 0.003.dcc).explicitGet()
       val buyOrder  = Order.buy(TxVersion.V2, buyer, matcher.publicKey, pair, 100000000L, 1, ts, ts + 1.day.toMillis, 0.003.dcc).explicitGet()
 
-      ExchangeTransaction
-        .signed(TxVersion.V2, matcher.privateKey, buyOrder, sellOrder, 100000000, 1, 0.003.dcc, 0.003.dcc, 0.011.dcc, ts)
-        .explicitGet()
+      TxHelpers.exchange(buyOrder, sellOrder, matcher, 100000000, 1, 0.003.waves, 0.003.waves, 0.011.waves, ts)
     }
 
     setScripts ++ txs ++ extxs

node-it/src/test/scala/com/decentralchain/it/sync/smartcontract/Ride4DAppsActivationTestSuite.scala
@@ -1,18 +1,19 @@
 package com.decentralchain.it.sync.smartcontract
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.Config
 import com.decentralchain.common.state.ByteStr
 import com.decentralchain.common.utils.EitherExt2.*
+import com.decentralchain.features.BlockchainFeatures
 import com.decentralchain.it.NodeConfigs
 import com.decentralchain.it.api.SyncHttpApi.*
 import com.decentralchain.it.sync.*
 import com.decentralchain.it.transactions.BaseTransactionSuite
 import com.decentralchain.lang.v1.estimator.v2.ScriptEstimatorV2
-import com.decentralchain.test.*
-import com.decentralchain.transaction.{Asset, AssetIdLength}
-import com.decentralchain.transaction.smart.script.ScriptCompiler
-import org.scalatest.CancelAfterFailure
 import com.decentralchain.state.Height
+import com.decentralchain.test.*
+import com.decentralchain.transaction.smart.script.ScriptCompiler
+import com.decentralchain.transaction.{Asset, AssetIdLength}
+import org.scalatest.CancelAfterFailure
 
 import scala.concurrent.duration.*
 
@@ -182,18 +183,6 @@ class Ride4DAppsActivationTestSuite extends BaseTransactionSuite with CancelAfte
 object Ride4DAppsActivationTestSuite {
   val activationHeight = Height(15)
 
-  val configWithRide4DAppsFeature = NodeConfigs.newBuilder
-    .withDefault(1)
-    .withSpecial(1, _.nonMiner)
-    .buildNonConflicting()
-    .map(
-      ConfigFactory
-        .parseString(
-          s"""dcc.blockchain.custom.functionality {
-             |  pre-activated-features.11 = ${activationHeight - 1}
-             |}""".stripMargin
-        )
-        .withFallback(_)
-    )
-
+  import NodeConfigs.*
+  val configWithRide4DAppsFeature = Seq(BiggestMiner, NotMiner).map(_.preactivatedFeatures((BlockchainFeatures.Ride4DApps, activationHeight - 1)))
 }

node-it/src/test/scala/com/decentralchain/it/sync/transactions/UpdateAssetInfoTransactionSuite.scala
@@ -15,6 +15,7 @@ import com.decentralchain.lang.directives.values.V4
 import com.decentralchain.lang.v1.compiler.{Terms, TestCompiler}
 import com.decentralchain.lang.v1.estimator.v3.ScriptEstimatorV3
 import com.decentralchain.state.Height
+import com.decentralchain.transaction.Asset.IssuedAsset
 import com.decentralchain.transaction.assets.IssueTransaction.{MaxAssetDescriptionLength, MaxAssetNameLength, MinAssetNameLength}
 import com.decentralchain.transaction.smart.script.ScriptCompiler
 import com.decentralchain.transaction.{TransactionType, TxVersion}
@@ -118,9 +119,8 @@ class UpdateAssetInfoTransactionSuite extends BaseTransactionSuite with CancelAf
         UpdateAssetInfoRequest(
           TxVersion.V1,
           AddressScheme.current.chainId,
-          None,
-          Some(issuer.publicKey.toString),
-          assetId,
+          issuer.publicKey.toString,
+          IssuedAsset(ByteStr(new Array[Byte](32))),
           "test",
           "test",
           None,

node/src/main/scala/com/decentralchain/transaction/assets/ReissueTransaction.scala
@@ -1,10 +1,11 @@
 package com.decentralchain.transaction.assets
 
-import com.decentralchain.account.{AddressScheme, KeyPair, PrivateKey, PublicKey}
+import com.decentralchain.account.{AddressScheme, PrivateKey, PublicKey}
+import com.decentralchain.common.state.ByteStr
 import com.decentralchain.crypto
 import com.decentralchain.lang.ValidationError
-import com.decentralchain.transaction.Asset.IssuedAsset
 import com.decentralchain.transaction.*
+import com.decentralchain.transaction.Asset.IssuedAsset
 import com.decentralchain.transaction.serialization.impl.ReissueTxSerializer
 import com.decentralchain.transaction.validation.TxValidator
 import com.decentralchain.transaction.validation.impl.ReissueTxValidator
@@ -26,14 +27,17 @@ case class ReissueTransaction(
 ) extends Transaction(TransactionType.Reissue, Seq(asset))
     with Versioned.ToV3
     with ProvenTransaction
-    with SigProofsSwitch
-    with TxWithFee.InDcc
+    with HasSignature
+    with TxWithFee.InWaves
     with FastHashId
     with PBSince.V3 {
+  override type T = ReissueTransaction
 
   override val bodyBytes: Coeval[Array[Byte]] = Coeval.evalOnce(ReissueTxSerializer.bodyBytes(this))
   override val bytes: Coeval[Array[Byte]]     = Coeval.evalOnce(ReissueTxSerializer.toBytes(this))
   override val json: Coeval[JsObject]         = Coeval.evalOnce(ReissueTxSerializer.toJson(this))
+
+  override def addProof(proof: ByteStr): ReissueTransaction = copy(proofs = this.proofs.add(proof))
 }
 
 object ReissueTransaction extends TransactionParser {
@@ -56,7 +60,7 @@ object ReissueTransaction extends TransactionParser {
       reissuable: Boolean,
       fee: Long,
       timestamp: Long,
-      proofs: Proofs,
+      proofs: Proofs = Proofs.empty,
       chainId: Byte = AddressScheme.current.chainId
   ): Either[ValidationError, ReissueTransaction] =
     for {
@@ -64,29 +68,4 @@ object ReissueTransaction extends TransactionParser {
       quantity <- TxPositiveAmount(quantity)(TxValidationError.NonPositiveAmount(quantity, "assets"))
       tx       <- ReissueTransaction(version, sender, asset, quantity, reissuable, fee, timestamp, proofs, chainId).validatedEither
     } yield tx
-
-  def signed(
-      version: TxVersion,
-      sender: PublicKey,
-      asset: IssuedAsset,
-      quantity: Long,
-      reissuable: Boolean,
-      fee: Long,
-      timestamp: Long,
-      signer: PrivateKey,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ReissueTransaction] =
-    create(version, sender, asset, quantity, reissuable, fee, timestamp, Nil, chainId).map(_.signWith(signer))
-
-  def selfSigned(
-      version: TxVersion,
-      sender: KeyPair,
-      asset: IssuedAsset,
-      quantity: Long,
-      reissuable: Boolean,
-      fee: Long,
-      timestamp: Long,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ReissueTransaction] =
-    signed(version, sender.publicKey, asset, quantity, reissuable, fee, timestamp, sender.privateKey, chainId)
 }

node/src/main/scala/com/decentralchain/transaction/assets/exchange/ExchangeTransaction.scala
@@ -1,6 +1,7 @@
 package com.decentralchain.transaction.assets.exchange
 
 import com.decentralchain.account.{AddressScheme, PrivateKey, PublicKey}
+import com.decentralchain.common.state.ByteStr
 import com.decentralchain.crypto
 import com.decentralchain.lang.ValidationError
 import com.decentralchain.transaction.*
@@ -26,13 +27,17 @@ case class ExchangeTransaction(
     timestamp: Long,
     proofs: Proofs,
     chainId: Byte
-) extends Transaction(TransactionType.Exchange, order1.assetPair.checkedAssets)
-    with Versioned.ToV3
-    with ProvenTransaction
-    with TxWithFee.InDcc
-    with FastHashId
-    with SigProofsSwitch
-    with PBSince.V3 {
+) extends Transaction(TransactionType.Exchange, order1.assetPair.checkedAssets),
+      ProvenTransaction,
+      HasSignature,
+      Versioned.ToV3,
+      TxWithFee.InWaves,
+      FastHashId,
+      PBSince.V3 {
+
+  override type T = ExchangeTransaction
+
+  override def addProof(proof: ByteStr): ExchangeTransaction = copy(proofs = proofs.add(proof))
 
   val (buyOrder, sellOrder) = if (order1.orderType == OrderType.BUY) (order1, order2) else (order2, order1)
 
@@ -98,19 +103,4 @@ object ExchangeTransaction extends TransactionParser {
         chainId
       ).validatedEither
     } yield tx
-
-  def signed(
-      version: TxVersion,
-      matcher: PrivateKey,
-      order1: Order,
-      order2: Order,
-      amount: Long,
-      price: Long,
-      buyMatcherFee: Long,
-      sellMatcherFee: Long,
-      fee: Long,
-      timestamp: Long,
-      chainId: Byte = AddressScheme.current.chainId
-  ): Either[ValidationError, ExchangeTransaction] =
-    create(version, order1, order2, amount, price, buyMatcherFee, sellMatcherFee, fee, timestamp, Proofs.empty, chainId).map(_.signWith(matcher))
 }